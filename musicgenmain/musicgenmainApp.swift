2fef01ab2ab520212cf9c4294684d3a178f56667
etch <hesham@beyondmngmt.com>
2025-06-15 15:28:30 +0400

    Initial Commit

---
+++ musicgenmain/musicgenmainApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicgenmainApp.swift
+//  musicgenmain
+//
+//  Created by ETCH on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct musicgenmainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musicgenmainTests/musicgenmainTests.swift
@@ -0,0 +1,16 @@
+//
+//  musicgenmainTests.swift
+//  musicgenmainTests
+//
+//  Created by ETCH on 15/06/2025.
+//
+
+import Testing
+
+struct musicgenmainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
